8527d8b121039c9554a92599801dcde0996f2d85
Alec Pedersen <alecsorenpedersen@gmail.com>
2022-10-09 11:32:25 +0100

    Finished Basic state machine

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './App.css';
 import { useMachine } from '@xstate/react';
 import { trafficLightMachine } from './trafficLightMachine';
 
-function App() {
+export const App = () => {
 	const [current, send] = useMachine(trafficLightMachine);
+
 	return (
 		<div className='container'>
 			<div className='pole' />
@@ -13,23 +14,25 @@ function App() {
 					type='radio'
 					readOnly
 					className='light red'
-					checked={current.matches('red')}
+					checked={current.matches({ ON: 'red' })}
 				/>
 				<input
 					type='radio'
 					readOnly
 					className='light yellow'
-					checked={current.matches('yellow')}
+					checked={current.matches({ ON: 'yellow' })}
 				/>
 				<input
 					type='radio'
 					readOnly
 					className='light green'
-					checked={current.matches('green')}
+					checked={current.matches({ ON: 'green' })}
 				/>
 				<button onClick={() => send('NEXT')}>NEXT</button>
+				<button onClick={() => send('TURN_ON')}>TURN ON</button>
+				<button onClick={() => send('TURN_OFF')}>TURN OFF</button>
 			</div>
 		</div>
 	);
-}
+};
 export default App;
--- src/trafficLightMachine.ts
+++ src/trafficLightMachine.ts
@@ -1,37 +1,50 @@
 import { createMachine } from 'xstate';
 
-type TrafficLightEvent = { type: 'NEXT' };
+type TrafficLightEvent =
+	| { type: 'NEXT' }
+	| { type: 'TURN_OFF' }
+	| { type: 'TURN_ON' };
 
 type TraffiLightState =
-	| { value: 'green'; context: undefined }
-	| { value: 'yellow'; context: undefined }
-	| { value: 'red'; context: undefined };
+	| { value: { ON: 'green' }; context: undefined }
+	| { value: { ON: 'yellow' }; context: undefined }
+	| { value: { ON: 'red' }; context: undefined }
+	| { value: 'OFF'; context: undefined };
 
 export const trafficLightMachine = createMachine<
-	undefined, // We don’t have context, so we type it as undefined
+	undefined,
 	TrafficLightEvent,
 	TraffiLightState
 >({
 	id: 'trafficLight',
-	initial: 'red',
+	initial: 'OFF',
 	states: {
-		green: {
-			on: { NEXT: 'red' },
-			after: {
-				3500: 'red',
+		ON: {
+			on: { TURN_OFF: 'OFF' },
+			initial: 'red',
+			states: {
+				green: {
+					on: { NEXT: 'red' },
+					after: {
+						500: 'red',
+					},
+				},
+				red: {
+					on: { NEXT: 'yellow' },
+					after: {
+						500: 'yellow',
+					},
+				},
+				yellow: {
+					on: { NEXT: 'green' },
+					after: {
+						500: 'green',
+					},
+				},
 			},
 		},
-		red: {
-			on: { NEXT: 'yellow' },
-			after: {
-				3500: 'yellow',
-			},
-		},
-		yellow: {
-			on: { NEXT: 'green' },
-			after: {
-				3500: 'green',
-			},
+		OFF: {
+			on: { TURN_ON: 'ON' },
 		},
 	},
 });
